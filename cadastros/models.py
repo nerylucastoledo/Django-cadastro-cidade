a3e50b75dbb0d07c7297c7045b690a1accd0592b
nerylucastoledo <lucasnerytoledo@hotmail.com>
2021-02-19 12:18:50 -0300

    Cadastra e remove cidades

--- cadastros/models.py
+++ cadastros/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Cidade(models.Model):
 
     #criação dos campos que Cidade irá conter
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, unique=True)
     capital = models.BooleanField(default=False)
 
     def __str__(self):
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from cadastros.forms import CidadeForm
 from cadastros.models import Cidade
 
 
 def lista_cidades(request):
-
-    qs = Cidade.objects.all()
+    qs = Cidade.objects.all().order_by('nome')
 
     context = {
         'cidades': qs,
@@ -16,7 +16,6 @@ def lista_cidades(request):
 
 
 def detalhe_cidade(request, id):
-
     # tenta pegar o objeto ou retorna o erro 404
     # ex: cidade = Cidade.objects.get
     cidade = get_object_or_404(Cidade, pk=id)
@@ -26,3 +25,32 @@ def detalhe_cidade(request, id):
     }
 
     return render(request, 'cadastros/detalhe_cidades.html', context)
+
+
+def cadastra_cidade(request):
+
+    if request.method == 'POST':
+        form = CidadeForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('cidades-list')
+
+    else:
+
+        form = CidadeForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'cadastros/cadastra_cidades.html', context)
+
+
+def remove_cidade(request, id):
+
+    cidade = get_object_or_404(Cidade, pk=id)
+
+    cidade.delete()
+
+    return redirect('cidades-list')
